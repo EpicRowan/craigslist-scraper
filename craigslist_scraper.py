7d7f8af8ffa6c261c8e36b8d75d92e43ac553f6e
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-21 18:50:42 -0700

    Minor page fix

--- craigslist_scraper.py
+++ craigslist_scraper.py
@@ -51,7 +51,7 @@ radius="5"
 
 scraper = CraigslistScraper(postal,max_price,radius)
 scraper.load_craigslist_url()
-# scraper.extract_post_titles()
+scraper.extract_post_titles()
 scraper.extract_post_urls()
 scraper.quit()
 
